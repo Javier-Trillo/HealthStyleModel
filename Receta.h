d3b9d447917d99dccdbfcbc612668fe34a5459d6
willmeza <123610565+willmeza@users.noreply.github.com>
2023-01-26 20:17:27 -0500

    Update Receta.h

--- Receta.h
+++ Receta.h
@@ -1,19 +1,19 @@
 /**
  * Project Untitled
  */
-
-
 #ifndef _RECETA_H
 #define _RECETA_H
 
 #include "Farmaceutico.h"
+using namespace system;
 
-
-class Receta: public Farmaceutico {
-public: 
-    String Date;
-    String Description;
-    double Total;
-};
+namespace HealthyStyleModel{
+    class Receta: public Farmaceutico {
+    public: 
+        String^ Date;
+        String^ Description;
+        double Total;
+    };
+}
 
 #endif //_RECETA_H
