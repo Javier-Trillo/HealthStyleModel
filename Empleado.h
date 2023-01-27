d3afc50eedd272f30b7f86fbdde2f11ab8252263
Javier-Trillo <123604447+Javier-Trillo@users.noreply.github.com>
2023-01-27 18:11:47 -0500

    Update Empleado.h

--- Empleado.h
+++ Empleado.h
@@ -6,16 +6,16 @@
 #pragma once
 
 #include "Person.h"
-
-
-public ref class Empleado: public Person{
-public: 
-    property String^ Username;
-    property String^ Password;
-    property char Gender;
-    property String^ Birthday;
-    property String^ Schedule;
-    property int YearsOfWork;
-};
-
+using namespace System;
+namespace HealthStyleModel{
+    public ref class Empleado: public Person{
+    public: 
+        property String^ Username;
+        property String^ Password;
+        property char Gender;
+        property String^ Birthday;
+        property String^ Schedule;
+        property int YearsOfWork;
+    };
+}
 
