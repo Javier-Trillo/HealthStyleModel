bb204c12959c6f33b95ed8056ad0108f3ce4c7c5
Javier-Trillo <123604447+Javier-Trillo@users.noreply.github.com>
2023-01-26 20:14:29 -0500

    Update Cita.h

--- Cita.h
+++ Cita.h
@@ -8,9 +8,9 @@ using namespace System;
 namespace HealthStyleModel{
     public ref class Cita{
     public: 
-        property string Date;
-        porperty string Speciality;
-        property string Reason;
+        property string^ Date;
+        porperty string^ Speciality;
+        property string^ Reason;
     };
 };
 
